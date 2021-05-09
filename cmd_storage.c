f26c24476f09ae12fa1ca5540a973d523a40d4e2
TomekTTX <tomekttx@gmail.com>
2021-05-09 23:25:37 +0200

    Add comments

--- cmd_storage.c
+++ cmd_storage.c
@@ -5,25 +5,32 @@
 
 #pragma warning (disable: 5045)
 
+/*
+* Provides size and format for sscanf() for a given argument type
+* 
+* key - argument type, such as <INT> or <STRING>
+* 
+* returns - pointer to a struct containing the argument's format and size
+*/
 const arg_node_t *size_node_get(const char *key) {
     static const arg_node_t nodes[] = {
-        { "<SUBCMD>",   ">",        0               },
-        { "<VOID>",     "$null",    0               },
-        { "<CHAR>",     "%c",       sizeof(char)    },
-        { "<BYTE>",     "%hhd",      sizeof(char)   },
-        { "<UBYTE>",    "%hhu",     sizeof(uchar)   },
-        { "<UCHAR>",    "%hhu",     sizeof(uchar)   },
-        { "<SHORT>",    "%hd",       sizeof(short)  },
-        { "<USHORT>",   "%hu",      sizeof(ushort)  },
-        { "<INT>",      "%d",       sizeof(int)     },
-        { "<UINT>",     "%u",       sizeof(uint)    },
-        { "<LONG>",     "%ld",      sizeof(long)    },
-        { "<ULONG>",    "%lu",      sizeof(ulong)   },
-        { "<LLONG>",    "%lld",     sizeof(llong)   },
-        { "<ULLONG>",   "%llu",     sizeof(ullong)  },
-        { "<STRING>",   "%s",       sizeof(char *)  },
-        { "<PTR>",      "%p",       sizeof(void *)  },
-     // { "<CMD>",      "$cmd",     sizeof(void *)  },
+        { "<SUBCMD>",  ">",      0              },
+        { "<VOID>",    "$null",  0              },
+        { "<CHAR>",    "%c",     sizeof(char)   },
+        { "<UCHAR>",   "%hhu",   sizeof(uchar)  },
+        { "<BYTE>",    "%hhd",   sizeof(char)   },
+        { "<UBYTE>",   "%hhu",   sizeof(uchar)  },
+        { "<SHORT>",   "%hd",    sizeof(short)  },
+        { "<USHORT>",  "%hu",    sizeof(ushort) },
+        { "<INT>",     "%d",     sizeof(int)    },
+        { "<UINT>",    "%u",     sizeof(uint)   },
+        { "<LONG>",    "%ld",    sizeof(long)   },
+        { "<ULONG>",   "%lu",    sizeof(ulong)  },
+        { "<LLONG>",   "%lld",   sizeof(llong)  },
+        { "<ULLONG>",  "%llu",   sizeof(ullong) },
+        { "<STRING>",  "%s",     sizeof(char *) },
+        { "<PTR>",     "%p",     sizeof(void *) },
+     // { "<CMD>",     "$cmd",   sizeof(void *) },
     };
     static const arg_node_t err = { "<ERROR>", "$unknown", 0 };
 
@@ -43,11 +50,20 @@ const arg_node_t *size_node_get(const char *key) {
     return &err;
 }
 
+// Temporary; adds cmd to list
+// Currently exists only for logging reasons
 void cmd_merge_subcmd(ptr_arraylist_t *list, command_t *cmd) {
     DEBUG_ONLY(printf("[INFO] Merge called for %s\n", cmd->name);)
     arraylist_push(list, cmd);
 }
 
+/*
+* Fills a command's syntax array
+* Makes cross-recursive calls with cmd_make()/cmd_alloc() when handling subcommands
+* 
+* args - currently parsed command string
+* cmd  - pointer to a command whose syntax array is to be filled
+*/
 void cmd_syntax_parse(char *args, command_t *cmd) {
 
     for (uint i = 0, j = 0; i < cmd->arg_cnt; ++i) {
@@ -62,6 +78,14 @@ void cmd_syntax_parse(char *args, command_t *cmd) {
     }
 }
 
+/*
+* Creates a heap-allocated command struct
+* 
+* input - command string
+* proc  - a struct containing the command's function and static data
+* 
+* returns - pointer to the newly created command
+*/
 command_t *cmd_alloc(const char *input, cmd_proc_t proc) {
     command_t *ret = malloc(sizeof(command_t));
     if (ret) {
@@ -71,6 +95,15 @@ command_t *cmd_alloc(const char *input, cmd_proc_t proc) {
     return ret;
 }
 
+/*
+* Creates a stack-allocated command struct
+* Makes cross-recursive calls with cmd_syntax_parse() when handling subcommands
+*
+* str  - command string
+* proc - a struct containing the command's function and static data
+*
+* returns - the newly created command
+*/
 command_t cmd_make(const char *str, cmd_proc_t proc) {
     command_t ret = { 0 };
     char *args = (char *)str;
@@ -96,6 +129,12 @@ command_t cmd_make(const char *str, cmd_proc_t proc) {
     return ret;
 }
 
+/*
+* Frees all memory allocated by cmd_make() / cmd_alloc()
+* The command becomes unusable, obviously
+* 
+* cmd - pointer to a command that is to be deleted
+*/
 void cmd_destroy(command_t *cmd) {
     if (!cmd)
         return;
